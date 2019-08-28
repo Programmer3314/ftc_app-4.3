4ed0896d54d92faac2c0e38e884b85adc8de6407
crezeshox <fmusial@outlook.com>
2019-08-27 21:50:37 -0400

    Created JoystickAxis class and set up axis and radian scales.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
@@ -42,36 +42,25 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-/**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
+
 
 @TeleOp(name="Basic: First OpMode", group="Iterative Opmode")
 //@Disabled
 public class FirstOpMode extends MyRobot
 {
-    public MoveParameter moveParam = new MoveParameter();
+    public MoveParameter moveParam;
     public Orientation angles;
-    // Declare OpMode members.
+    public JoystickAxis forward;
+    public JoystickAxis turn;
+    public JoystickAxis strafe;
 
-    //public DcMotorEx leftClawMotor, rightClawMotor;
-    // -5 = start position, 6 = closed, -30 = opened
-
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
     public void OpModeInit() {
+        moveParam = new MoveParameter();
+        forward = new JoystickAxis(JoystickAxis.Axis.leftY, gamepad1).setScale(-10);
+        turn = new JoystickAxis(JoystickAxis.Axis.rightX, gamepad1).setScale(-10);
+        strafe = new JoystickAxis(JoystickAxis.Axis.leftX, gamepad1).setScale(-10);
+
 
     }
     /*
@@ -98,20 +87,12 @@ public class FirstOpMode extends MyRobot
         double leftPower;
         double rightPower;
         angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
 
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
 
         // Send calculated power to wheels
-        moveParam.strafe = -gamepad1.left_stick_x *30;
-        moveParam.forward = gamepad1.left_stick_y *30;
-        moveParam.turn = gamepad1.right_stick_x *30;
+        moveParam.strafe = strafe.getValue();
+        moveParam.forward = forward.getValue();
+        moveParam.turn = turn.getValue();
         moveParam.mode = MoveParameter.Modes.Velocity;
         train1.move(moveParam);
 
